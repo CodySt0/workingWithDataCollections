1f796995eb59fab38842fbbb5fa4e387f3f619f4
CodySt0 <Codistgeorge@gmail.com>
2024-06-09 19:46:16 -0700

    added more and more js

---
+++ index.js
@@ -0,0 +1,6 @@
+let csv =
+  "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+let newArray = csv.split("\n");
+console.log(csv);
+
+let dataArray = [];
